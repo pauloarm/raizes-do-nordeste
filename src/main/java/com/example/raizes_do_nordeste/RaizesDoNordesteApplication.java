2701d1e525e9fa5275a9169485fe94e3139b9d09
Paulo Machado <paulormachado95@gmail.com>
2026-05-09 10:37:17 -0300

    first commit

---
+++ src/main/java/com/example/raizes_do_nordeste/RaizesDoNordesteApplication.java
@@ -0,0 +1,13 @@
+package com.example.raizes_do_nordeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizesDoNordesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaizesDoNordesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/raizes_do_nordeste/RaizesDoNordesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raizes_do_nordeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesDoNordesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
